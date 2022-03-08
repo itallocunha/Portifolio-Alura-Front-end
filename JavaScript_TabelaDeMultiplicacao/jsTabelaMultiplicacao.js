61d3830570aa959ac40505c45c22067526bc00d4
itallocunha <80712552+itallocunha@users.noreply.github.com>
2022-03-08 17:23:15 -0300

    Atualizacao

---
+++ JavaScript_TabelaDeMultiplicacao/jsTabelaMultiplicacao.js
@@ -0,0 +1,10 @@
+function criarTabela() { // Substitua "nomeDaSuaFuncao" por um nome mais apropriado
+    // todo: escrever dois loops aninhados para desenhar o array bidimensional e monstar o restante da tabela no console.
+    let x = [];
+
+    for (let i = 0; i <= 10; i++) {
+        x[i] = [];
+    }
+     console.table(x)
+  
+}
