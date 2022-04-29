84bc723c9df38c9ec642cb3d3009ae339c7da501
itallocunha <80712552+itallocunha@users.noreply.github.com>
2022-04-29 13:27:59 -0300

    Animação
    Adicionando animação e preloading.

--- Blue Journey/Assets/JavaScript/main.js
+++ Blue Journey/Assets/JavaScript/main.js
@@ -1,5 +1,18 @@
+//DDeclarando variaveis
 var btnContact = document.querySelector('.bj-btncontact');
 
+//Page preloader
+window.addEventListener('load', function(){
+     var pagePreloader = document.querySelector('.bj-preloader');
+     pagePreloader.classList.add('bj-fade-out');
+
+     this.setTimeout(function(){
+          pagePreloader.style.display = 'none';
+     },2000);
+});
+
+
+//Abrindo e fechando informação de contato
 btnContact.addEventListener('click',function(){
      var boxContact = document.querySelector('.bj-contact-info');
      boxContact.classList.toggle('bj-is-open');
